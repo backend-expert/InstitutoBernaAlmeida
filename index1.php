16ddc31892e769a3449a2d3222d1d6d043786d7b
Isaque Menezes da Silva <isaque-menezes@hotmail.com>
2021-12-15 17:36:34 -0300

    arquivos upload

---
+++ index1.php
@@ -0,0 +1,12 @@
+<?php 
+	include "config/config.php";
+	include (DIRREQ."core/templates/header.php"); 
+
+?>
+
+	<h1>Hello New </h1>
+
+<?php 
+	
+	include (DIRREQ."core/templates/footer.php"); 
+?>
