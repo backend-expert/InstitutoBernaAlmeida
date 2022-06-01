1e56122e19911f466f960831b414cb23842ee97d
Isaque Menezes <isaque-menezes@hotmail.com>
2022-06-01 10:45:33 -0300

    adicao de dados bancarios e front end page doacao

--- core/templates/section-doacao.php
+++ core/templates/section-doacao.php
@@ -1,23 +1,34 @@
 <section class="about section" id="doacao">
-                <h2 class="section-title">Doação</h2>
+              
+                <h2 class="section-title">“Ajude pessoas com demência a viverem de forma digna Doe agora!”.</h2>
+
 
                 <div class="about__container bd-grid">
-                    <div class="about__img">
-                        <img src="<?php echo DIRIMG.'logo_small_150x150.jpg'; ?>" alt="logo do Instituto Berna Almeida"> 
-                    </div>
+                    
+                        <img src="<?php echo DIRIMG.'logotipo Berna Almeida Horizontal (3).png'; ?>" alt="logo do Instituto Berna Almeida"> 
+                  
 
                     <div>
-                        <h2 class="about__subtitle">“Ajude pessoas com demência a viverem de forma digna.</h2>
-                        <span class="about__profession"> Doe agora!”.</span>
-                        
-                        <p class="about__text">Somos uma ONG de assistência a pessoas com deficiência mental, Alzheimer, demências em geral. </p>
+                        <!-- <span class="about__profession"> </span> -->
+                        <div class="about__img">
+                        <img src="<?php echo DIRIMG.'caixa.png'; ?>" alt="logo do Instituto Berna Almeida"> 
+                    </div>
+                    
+                        <p class="about__text"><br> Dados Bacários <br>
+                        Banco: <b>104 Caixa Economica Federal</b> <br>
+                        Agência: <b>1985</b> <br>
+                        Tipo:  <b>013 Poupança</b> <br>
+                         
+                        Número da Conta:  <b>00020741-2  </b>
+                        </p>
+            
                         
                         <p>E se você tiver alguma dúvida, não deixe de entrar em contato com nossa contabilidade, que certamente podemos te ajudar em qualquer parte financeira, tributária ou fiscal.
 
 </p>
                         <br>
                         
-                        <span class="about__profession"> Um forte abraço! Conecte-se conosco.</span>
+                        <span class="about__profession"> Conecte-se conosco.</span>
                        
                         <div class="about__social">
                             <a href="https://www.facebook.com/institutobernalmeida" target="_blank" class="about__social-icon"><i class="bx bxl-facebook-square"></i></a>
