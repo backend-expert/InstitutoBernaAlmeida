2c869859a6d9a9d1f0aed1730f81e62673edd22a
Isaque Menezes <isaque-menezes@hotmail.com>
2022-06-01 18:28:45 -0300

    responsive doacao 3 e link header

--- core/templates/header.php
+++ core/templates/header.php
@@ -1,21 +1,22 @@
 <header class="l-header">
             <nav class="nav bd-grid">
                 <div>
-                    <a href="#" class="nav__logo">
+                    <a href="<?php echo DIRPAGE.'index.php';?>" class="nav__logo">
                        Instituto Berna Almeida
                    </a> 
                 </div>
 
                 <div class="nav__menu" id="nav-menu">
                     <ul class="nav__list">
-                        <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
-                        <li class="nav__item"><a href="#about" class="nav__link">Sobre</a></li>
+                        <li class="nav__item"><a href="<?php echo DIRPAGE.'index.php#home'; ?>" class="nav__link active">Home</a></li>
+                        <li class="nav__item"><a href="<?php echo DIRPAGE.'index.php#about'; ?>" class="nav__link">Sobre</a></li>
                         <li class="nav__item"><a href="https://www.institutobernalmeida.com.br/blog" target="_blank" class="nav__link">Postagens</a></li>
-                        <li class="nav__item"><a href="#projects" class="nav__link">Projetos</a></li>
+                        <li class="nav__item"><a href="<?php echo DIRPAGE.'index.php#projects'; ?>" class="nav__link">Projetos</a></li>
                         <li class="nav__item"><a href="<?php echo DIRPAGE.'doacao.php#doacao';?>" class="nav__link">Fazer uma Doação</a></li>
                     </ul>
                 </div>
 
+                <!-- responsive mobile toggle  -->
                 <div class="nav__toggle" id="nav-toggle">
                     <i class='bx bx-menu'></i>
                 </div>
