5c8e928e0e2e2e23ce19368f5c896f4b499a33bf
Isaque Silva <isaque.silva@equilibriumweb.com>
2023-05-27 12:36:04 -0300

    update

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+    #Caminhos absolutos
+    if($_SERVER['HTTP_HOST'] === 'localhost')
+    {
+        $pastaInterna="github/InstitutoBernaAlmeida/";
+
+    } else {
+        $pastaInterna = "/";
+      
+    }
+
+
+    define('DIRPAGE',"http://{$_SERVER['HTTP_HOST']}/{$pastaInterna}");
+    (substr($_SERVER['DOCUMENT_ROOT'],-1)=='/')?$barra="":$barra="/";
+    define('DIRREQ',"{$_SERVER['DOCUMENT_ROOT']}{$barra}{$pastaInterna}");
+
+    #Atalhos
+    define('DIRCSS', DIRPAGE.'core/css/');
+    define('DIRIMG', DIRPAGE.'core/images/');
+    define('DIRJS', DIRPAGE.'core/js/');
+
+    
+    #Outros
+    define("DOMAIN",$_SERVER["HTTP_HOST"]);
